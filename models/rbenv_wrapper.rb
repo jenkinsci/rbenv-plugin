17a71b4f54d5a40f8daaba79738a696ebe21e544
Yamashita Yuu <yamashita@geishatokyo.com>
2013-07-22 19:25:42 +0900

    Use model constants from view

--- models/rbenv_wrapper.rb
+++ models/rbenv_wrapper.rb
@@ -4,6 +4,15 @@ require "rbenv/rack"
 require "jenkins/rack"
 
 class RbenvDescriptor < Jenkins::Model::DefaultDescriptor
+  DEFAULT_VERSION = "1.9.3p429"
+  DEFAULT_GEM_LIST = "bundler,rake"
+  DEFAULT_IGNORE_LOCAL_VERSION = false
+  DEFAULT_RBENV_ROOT = "$HOME/.rbenv"
+  DEFAULT_RBENV_REPOSITORY = "git://github.com/sstephenson/rbenv.git"
+  DEFAULT_RBENV_REVISION = "master"
+  DEFAULT_RUBY_BUILD_REPOSITORY = "git://github.com/sstephenson/ruby-build.git"
+  DEFAULT_RUBY_BUILD_REVISION = "master"
+
   include Jenkins::RackSupport
   def call(env)
     Rbenv::RackApplication.new.call(env)
@@ -22,15 +31,10 @@ class RbenvWrapper < Jenkins::Tasks::BuildWrapper
   describe_as Java.hudson.tasks.BuildWrapper, :with => RbenvDescriptor
   display_name "rbenv build wrapper"
 
-  # FIXME: these values should be shared between views/rbenv_wrapper/config.erb
-  DEFAULT_VERSION = "1.9.3p429"
-  DEFAULT_GEM_LIST = "bundler,rake"
-  DEFAULT_IGNORE_LOCAL_VERSION = false
-  DEFAULT_RBENV_ROOT = "$HOME/.rbenv"
-  DEFAULT_RBENV_REPOSITORY = "git://github.com/sstephenson/rbenv.git"
-  DEFAULT_RBENV_REVISION = "master"
-  DEFAULT_RUBY_BUILD_REPOSITORY = "git://github.com/sstephenson/ruby-build.git"
-  DEFAULT_RUBY_BUILD_REVISION = "master"
+  # The default values should be set on both instantiation and deserialization.
+  def initialize(attrs={})
+    from_hash(attrs)
+  end
 
   attr_reader :build
   attr_reader :launcher
@@ -44,11 +48,6 @@ class RbenvWrapper < Jenkins::Tasks::BuildWrapper
   attr_accessor :rbenv_revision
   attr_accessor :ruby_build_repository
   attr_accessor :ruby_build_revision
-
-  # The default values should be set on both instantiation and deserialization.
-  def initialize(attrs={})
-    from_hash(attrs)
-  end
 
   # Will be invoked by jruby-xstream after deserialization from configuration file.
   def read_completed()
@@ -77,14 +76,14 @@ class RbenvWrapper < Jenkins::Tasks::BuildWrapper
 
   private
   def from_hash(hash={})
-    @version = string(hash.fetch("version", @version), DEFAULT_VERSION)
-    @gem_list = string(hash.fetch("gem_list", @gem_list), DEFAULT_GEM_LIST)
-    @ignore_local_version = boolean(hash.fetch("ignore_local_version", @ignore_local_version), DEFAULT_IGNORE_LOCAL_VERSION)
-    @rbenv_root = string(hash.fetch("rbenv_root", @rbenv_root), DEFAULT_RBENV_ROOT)
-    @rbenv_repository = string(hash.fetch("rbenv_repository", @rbenv_repository), DEFAULT_RBENV_REPOSITORY)
-    @rbenv_revision = string(hash.fetch("rbenv_revision", @rbenv_revision), DEFAULT_RBENV_REVISION)
-    @ruby_build_repository = string(hash.fetch("ruby_build_repository", @ruby_build_repository), DEFAULT_RUBY_BUILD_REPOSITORY)
-    @ruby_build_revision = string(hash.fetch("ruby_build_revision", @ruby_build_revision), DEFAULT_RUBY_BUILD_REVISION)
+    @version = string(hash.fetch("version", @version), RbenvDescriptor::DEFAULT_VERSION)
+    @gem_list = string(hash.fetch("gem_list", @gem_list), RbenvDescriptor::DEFAULT_GEM_LIST)
+    @ignore_local_version = boolean(hash.fetch("ignore_local_version", @ignore_local_version), RbenvDescriptor::DEFAULT_IGNORE_LOCAL_VERSION)
+    @rbenv_root = string(hash.fetch("rbenv_root", @rbenv_root), RbenvDescriptor::DEFAULT_RBENV_ROOT)
+    @rbenv_repository = string(hash.fetch("rbenv_repository", @rbenv_repository), RbenvDescriptor::DEFAULT_RBENV_REPOSITORY)
+    @rbenv_revision = string(hash.fetch("rbenv_revision", @rbenv_revision), RbenvDescriptor::DEFAULT_RBENV_REVISION)
+    @ruby_build_repository = string(hash.fetch("ruby_build_repository", @ruby_build_repository), RbenvDescriptor::DEFAULT_RUBY_BUILD_REPOSITORY)
+    @ruby_build_revision = string(hash.fetch("ruby_build_revision", @ruby_build_revision), RbenvDescriptor::DEFAULT_RUBY_BUILD_REVISION)
   end
 
   # Jenkins may return empty string as attribute value which we must ignore
